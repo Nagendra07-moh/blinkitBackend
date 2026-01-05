cc40df2d6585949438bccf2dd183d2fba6d1aabd
Nagendra <nagendra.mohan.5@gmail.com>
2026-01-06 04:26:50 +0530

    Some order Schema Added

--- src/modles/order.js
+++ src/modles/order.js
@@ -2,8 +2,33 @@ import mongoose from "mongoose";
 
 
 const orderSchema = new mongoose.Schema({
-    orderId: { type: String, unique: true },
-    customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer", required: true },
-    deliveryPartner: { type: mongoose.Schema.Types.ObjectId, ref: "DeliveryPartner" },
-    branch: { type: mongoose.Schema.Types.ObjectId, ref: "Branch", required: true }
+    orderId: {
+        type: String,
+        unique: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Customer", required: true
+    },
+    deliveryPartner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "DeliveryPartner"
+    },
+    branch: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Branch", required: true
+    },
+    items:[
+        {
+            id:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Product",
+                required:true
+            },
+            item:{
+                type:mongoose.Schema.Types.ObjectId,
+                
+            }
+        }
+    ]
 })
